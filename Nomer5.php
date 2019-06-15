a95446abf1f2281fdd427ad9e2789b78db9086a6
muhRobai <50520431+muhRobai@users.noreply.github.com>
2019-06-16 06:28:44 +0700

    Add files via upload

---
+++ Nomer5.php
@@ -0,0 +1,41 @@
+<?php
+function Kata($kalimat,$kata){
+        $kata1 = $kalimat;
+        $kata2= strrev($kata1);
+        $cari = $kata;
+        $jumlah_kata = 0;
+        $arr_kata1 = str_split($kata1);
+        $arr_kata2 = str_split($kata2);
+        $arr_cari = str_split($cari);
+        $jml_kata = count($arr_kata2);
+        $jml_cari = count($arr_cari);
+
+        for ($i = 0; $i <= ($jml_kata - $jml_cari); $i++) {
+            $kata = "";
+            for ($j = $i; $j < ($i + $jml_cari); $j++) {
+                $kata .= $arr_kata1[$j];
+            }
+            if ($kata == $cari) {
+                $jumlah_kata += 1;
+            }
+
+        }
+
+
+        for ($i = 0; $i <= ($jml_kata - $jml_cari); $i++) {
+            $kata = "";
+            for ($j = $i; $j < ($i + $jml_cari); $j++) {
+                $kata .= $arr_kata2[$j];
+            }
+            if ($kata == $cari) {
+                $jumlah_kata += 1;
+            }
+
+        }
+        echo "data ditemukan " . $jumlah_kata . " kali";
+}
+
+Kata('banananana','nana');
+
+
+?>
